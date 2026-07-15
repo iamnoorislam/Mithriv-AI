10b371a8bde8da1289179deaaef2139b99a9c227
Noor Islam <noor.islam@veris.in>
2026-07-15 12:42:59 +0530

    chore: delete industries page and revert navigation link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -157,7 +157,7 @@ export default function Navbar() {
                     </div>
                 </li>
 
-                <li><Link href="/industries">Industries</Link></li>
+                <li><Link href="/#">Industries</Link></li>
                 <li className={`has-dropdown ${activeDropdown === 'resources' ? 'is-active' : ''}`}>
                     <a style={{ cursor: 'pointer' }} onClick={(e) => { e.preventDefault(); setActiveDropdown(activeDropdown === 'resources' ? null : 'resources'); }}>Resources <svg width="10" height="6" viewBox="0 0 10 6" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ opacity: 0.6, transition: 'transform 0.3s' }}>
                             <path d="M1 1L5 5L9 1" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
